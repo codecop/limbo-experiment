6e3d1c94d16cdc8fa76c5256e3a0f3a8a94310b6
Claus <claus.aichinger@mailbox.org>
2021-06-17 12:53:16 +0200

    [tcr] working

--- test_snake.py
+++ test_snake.py
@@ -2,10 +2,10 @@
 https://docs.google.com/document/d/1ac_DFYlnCyb23le62Tn4I9MRbUnKFIAk9y2lc9wMtR8/edit#heading=h.98eb2udzjo75
 
 Grundanforderungen:
-- es startet unten mitte und fÃ¤hrt nach oben
+- es startet unten mitte und fÃÂ¤hrt nach oben
 - beim Start hat die Schlanke 3 Glieder
 - alle n Ticks kommt ein Glied dazu
-- jeden âTickâ fÃ¤hrt es 1 Feld (und gibt frei)
+- jeden Ã¢ÂÂTickÃ¢ÂÂ fÃÂ¤hrt es 1 Feld (und gibt frei)
 - auf User Input kann es rechts/links drehen
 - trifft Snake sich selbst ist Spiel aus
 - trifft Snake eine Wand ist Spiel aus
@@ -15,15 +15,15 @@ Nice to have
 - Es kann ein Labyrint von Datei als Level geladen werden
 - Geschwindigkeit
 - Schwierigkeitsgrad
-- Essen verlÃ¤ngert Schlange sofort
+- Essen verlÃÂ¤ngert Schlange sofort
 
 Domain
 - Snake
   - Direction
   - Length?
   - Head, Tail
-  - âAdvanceâ
-  - âGrowthâ
+  - Ã¢ÂÂAdvanceÃ¢ÂÂ
+  - Ã¢ÂÂGrowthÃ¢ÂÂ
 - Arena
   - Obstacle
 - Tick
