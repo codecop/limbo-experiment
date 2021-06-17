b662eb7a3c0b2739125ff616446ba9acb6f58589
Claus <claus.aichinger@mailbox.org>
2021-06-17 14:01:33 +0200

    [tcr] working

--- test_snake.py
+++ test_snake.py
@@ -89,7 +89,7 @@ def snake():
 
 
 def test_there_is_a_snake(snake):
-    assert snake.direction == "N"
+    assert snake.d2 == Directions.NORTH
     assert snake.position == (0, 0)
     assert snake.body == [(0, 0), (0, -1), (0, -2)]
 
