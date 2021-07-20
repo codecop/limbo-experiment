589708b6ed826dbd485cca78bd113f351e25388a
Claus <claus.aichinger@mailbox.org>
2021-07-20 15:18:43 +0200

    [tcr] working

---
+++ test_game.py
@@ -0,0 +1,17 @@
+# -*- coding: latin-1 -*-
+"""
+Grundanforderungen:
+- X alle n Ticks kommt ein Glied dazu - von aussen gesteuert
+- X auf User Input kann es rechts/links drehen - von aussen gesteuert
+- X trifft Snake sich selbst ist Spiel aus - von aussen gesteuert
+- X trifft Snake eine Wand ist Spiel aus - von aussen gesteuert
+"""
+from test_snake import Point, Snake
+from test_arena import Arena
+from test_tk_view import TkView
+
+import pytest
+
+
+def test_foo():
+    pass
