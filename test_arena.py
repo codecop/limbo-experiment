5e876945ea8105de21fd2f924816e9e93f047ddd
Claus <claus.aichinger@mailbox.org>
2021-07-20 11:59:28 +0200

    [tcr] working

--- test_arena.py
+++ test_arena.py
@@ -43,6 +43,5 @@ def test_there_is_an_arena_with_walls(arena3x4):
     assert (-3, 3) in arena3x4.walls()
     assert (-3, 4) in arena3x4.walls()
 
-    assert (-3, 4) in arena3x4.walls()
-    assert (-3, 4) in arena3x4.walls()
-    assert (-3, 4) in arena3x4.walls()
+    assert (3, 4) in arena3x4.walls()
+    assert (3, -4) in arena3x4.walls()
