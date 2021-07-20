124aa7483951851e4efbf77ce8bcf5db315a77ea
Claus <claus.aichinger@mailbox.org>
2021-07-20 10:31:57 +0200

    [tcr] working

--- test_snake.py
+++ test_snake.py
@@ -165,7 +165,7 @@ def test_snake_grows(snake):
     snake.grow()
     snake.advance()
     assert snake.position == (0, 1)
-    # assert snake.body == [(0, 1), (0, 0), (0, -1), (0, -2)]
+    assert snake.body == [(0, 1), (0, 0), (0, -1), (0, -2)]
 
 
 # was passier bei 2x grow?
