9a255f28959eea3783fbc2ea9d3b74d196ef8fbc
Claus <claus.aichinger@mailbox.org>
2021-07-20 12:10:56 +0200

    [tcr] working

--- test_arena.py
+++ test_arena.py
@@ -61,6 +61,7 @@ def test_there_is_an_arena_with_walls_around(arena3x4):
     assert Point(3, -4) in arena3x4.walls()
 
     # TODO assert length
+    assert Point(0, 0) not in arena3x4.walls()
 
 
 def test_if_areana_position_occupied(arena3x4):
