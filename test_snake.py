9df76e0abd4375ffd5ff4081938d04a69c0c45f3
Claus <claus.aichinger@mailbox.org>
2021-07-20 11:12:04 +0200

    [tcr] working

--- test_snake.py
+++ test_snake.py
@@ -41,14 +41,6 @@ class Point(namedtuple("Point", ["x", "y"])):
         return Point(self.x + other.x, self.y + other.y)
 
 
-def add_elementwise(x, y):
-    z = []
-    for xi, yi in zip(x, y):
-        zi = xi + yi
-        z.append(zi)
-    return tuple(z)
-
-
 class Snake:
     def __init__(self):
         self.direction = Directions.NORTH
