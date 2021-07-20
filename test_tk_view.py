d98760c2877ddb579f4ceac8424f3fae6868cb65
Claus <claus.aichinger@mailbox.org>
2021-07-20 14:34:27 +0200

    [tcr] working

--- test_tk_view.py
+++ test_tk_view.py
@@ -28,18 +28,27 @@ import pytest
 
 # registerViewModel(model)
 # update
+RADIUS = 10
 
 
 class TkView:
     def __init__(self, root):
         self.window = root
         self.window.title("Snake")
-        canvas = Canvas(root, width=100, height=100, background="white")
+        self._width = 100
+        self._height = 100
+        canvas = Canvas(
+            root, width=self._width, height=self._height, background="white"
+        )
         canvas.pack()
         self.canvas = canvas
 
     def draw_snake(self, points):
         point = points[0]
+        x0 = self._width / 2 - RADIUS
+        x1 = self._width / 2 + RADIUS
+        y0 = self._height / 2 - RADIUS
+        y1 = self._height / 2 + RADIUS
         self.canvas.create_rectangle(0, 0, 1, 1, fill="green", tags=("snake"))
 
 
