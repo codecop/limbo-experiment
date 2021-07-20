e4765c3e5874cb4d776620b0849f7e7fcf79b917
Claus <claus.aichinger@mailbox.org>
2021-07-20 15:19:17 +0200

    [tcr] working

--- test_game.py
+++ test_game.py
@@ -6,6 +6,7 @@ Grundanforderungen:
 - X trifft Snake sich selbst ist Spiel aus - von aussen gesteuert
 - X trifft Snake eine Wand ist Spiel aus - von aussen gesteuert
 """
+from tkinter import Tk
 from test_snake import Point, Snake
 from test_arena import Arena
 from test_tk_view import TkView
@@ -15,3 +16,9 @@ import pytest
 
 def test_foo():
     pass
+
+
+if __name__ == "__main__":
+    root = Tk()
+    # view = TkView(root)
+    # root.mainloop()
