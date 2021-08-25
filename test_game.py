4ab7a589c69246d201bc0973cd24aeaf92f54de2
Claus <claus.aichinger@mailbox.org>
2021-08-25 11:58:28 +0200

    [tcr] working

--- test_game.py
+++ test_game.py
@@ -57,7 +57,7 @@ def test_game_tick_turn_left_rotates_and_moves_snake(game):
 
 
 def test_game_tick_turn_right_rotates_and_moves_snake(game):
-    turn_right = TurnCommand.LEFT
+    turn_right = TurnCommand.RIGHT
     game.tick(turn_right)
     snake_coordinates = game.snake()
     # assert snake_coordinates == [Point(1, 0), Point(0, 0), Point(0, -1)]
