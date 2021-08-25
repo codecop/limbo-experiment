e5b4df5d1f744bf7a0f2cd3c4612427d01f7651d
Claus <claus.aichinger@mailbox.org>
2021-08-25 11:36:20 +0200

    [tcr] working

--- test_game.py
+++ test_game.py
@@ -25,4 +25,4 @@ def test_there_is_a_game(game):
 def test_new_game_has_snake(game):
     snake_coordinates = game.snake
 
-    # assert snake.body == [Point(0, 0), Point(0, -1), Point(0, -2)]
+    # assert snake_coordinates == [Point(0, 0), Point(0, -1), Point(0, -2)]
