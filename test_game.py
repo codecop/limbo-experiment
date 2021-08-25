9b499317a2e8106ae68c62d345dca48109c77074
Claus <claus.aichinger@mailbox.org>
2021-08-25 11:36:56 +0200

    [tcr] working

--- test_game.py
+++ test_game.py
@@ -25,3 +25,7 @@ def test_there_is_a_game(game):
 def test_new_game_has_snake(game):
     snake_coordinates = game.snake()
     assert snake_coordinates == [Point(0, 0), Point(0, -1), Point(0, -2)]
+
+
+def test_new_game_has_arena(game):
+    pass
