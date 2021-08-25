a6dcd202781817e5a6e74981a9096af2ef5ac20e
Claus <claus.aichinger@mailbox.org>
2021-08-25 14:36:59 +0200

    [tcr] revert

--- test_presenter.py
+++ test_presenter.py
@@ -35,7 +35,6 @@ class Presenter:
 
     def _loop(self):
         self._game.tick(self._next_command)
-        self._next_command = None
         # draw snake and draw arena
         # reset command
 
@@ -63,7 +62,7 @@ def test_presenter_sets_command_left_only_once(viewGamePresenter):
     presenter._loop()
     game.tick.assert_called_with(TurnCommand.RIGHT)
     presenter._loop()
-    game.tick.assert_called_once_with(TurnCommand.RIGHT)
+    # game.tick.assert_called_once_with(TurnCommand.RIGHT)
 
 
 if __name__ == "__main__":
