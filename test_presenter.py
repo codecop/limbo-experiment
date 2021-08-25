e87b1c71ba93befd8da33dade68a85141029cebf
Claus <claus.aichinger@mailbox.org>
2021-08-25 14:50:46 +0200

    [tcr] working

--- test_presenter.py
+++ test_presenter.py
@@ -89,7 +89,7 @@ def test_presenter_start_schedules_loop_and_draws_stuff(viewGamePresenter):
     view, _, presenter = viewGamePresenter
     presenter.start()
     view.schedule_tick.assert_called()
-    # view.draw_snake.assert_called()
+    view.draw_snake.assert_called()
     # view.draw_arena.assert_called()
 
 
