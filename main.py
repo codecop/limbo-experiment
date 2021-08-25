431b60fadf896f1f97f5ebed1dcbce57e6bdfecc
Claus <claus.aichinger@mailbox.org>
2021-08-25 14:54:48 +0200

    [tcr] working

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from test_game import Game
+from test_arena import Arena
+from test_snake import Snake
 from tkinter import Tk
 from test_presenter import Presenter
 from test_tk_view import TkView
@@ -5,5 +8,10 @@ from test_tk_view import TkView
 if __name__ == "__main__":
     root = Tk()
     view = TkView(root)
+
+    snake = Snake()
+    arena = Arena(3, 4)
+    game = Game(snake, arena)
+
     Presenter(view, game)
     root.mainloop()
