5ed2da273e35aa423349ce9c7ee83ce430a75aaf
Claus <claus.aichinger@mailbox.org>
2021-08-25 11:35:58 +0200

    [tcr] failing

--- test_game.py
+++ test_game.py
@@ -1,6 +1,6 @@
 # -*- coding: latin-1 -*-
 
-from test_snake import Snake
+from test_snake import Point, Snake
 
 import pytest
 
@@ -24,5 +24,4 @@ def test_there_is_a_game(game):
 
 def test_new_game_has_snake(game):
     snake_coordinates = game.snake
-
-    # assert snake.body == [Point(0, 0), Point(0, -1), Point(0, -2)]
+    assert snake_coordinates == [Point(0, 0), Point(0, -1), Point(0, -2)]
