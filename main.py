e853b357245f1d509ac324e0a3580277edd6a62c
Claus <claus.aichinger@mailbox.org>
2021-08-25 14:53:07 +0200

    [tcr] working

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from test_presenter import Presenter
 
 
-# if __name__ == "__main__":
-#     root = Tk()
-#     view = TkView(root)
-#     game = Presenter(view)
-#     root.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    view = TkView(root)
+    game = Presenter(view)
+    root.mainloop()
