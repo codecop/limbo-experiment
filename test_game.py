334ad3f0bee8876de6107457bc873cb6f76c3250
Peter Kofler <peter.kofler@code-cop.org>
2022-03-04 14:25:08 +0100

    [tcr] revert

--- test_game.py
+++ test_game.py
@@ -20,8 +20,8 @@ class Game:
     def tick(self, command=None):
         if not self._is_running:
             return
-        if self._arena.is_position_apple(self._snake.head):
-            pass
+        #if self._arena.is_position_apple(self._snake.head):
+        #    pass
         self._handle_growth()
         self._handle_command(command)
         self._snake.advance()
