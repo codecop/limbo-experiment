8d883bc72bbd3cee698ba24a68bc626522992389
Claus Aichinger <claus.aichinger@mailbox.org>
2022-03-07 10:41:34 +0100

    Rename compare coverage to check

--- test_check_change.py
+++ test_check_change.py
@@ -2,7 +2,7 @@ from check_change import compute_lines_changed
 
 
 def test_compute_number_of_lines_changed():
-    output = "3\t0\tcompare_coverage.py\n3\t2\tconftest.py\n"
+    output = "3\t0\tcheck_coverage.py\n3\t2\tconftest.py\n"
     expected = (6, 2)
 
     assert compute_lines_changed(output) == expected
