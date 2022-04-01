676c9c424d1c1834b69bf8b7cee9a2177221b904
Claus Aichinger <claus.aichinger@mailbox.org>
2022-04-01 14:35:34 +0200

    [tcr] working

--- tests/game/test_game.py
+++ tests/game/test_game.py
@@ -24,6 +24,7 @@ def test_game_can_set_apples():
     arena = Arena(50, 50, initial_apple=Point(-1, 0))
     game = Game(snake, arena)
     assert len(game.apples()) == 1
+    game.tick()
     # for tick in range(10):
     #    game.tick()
 
