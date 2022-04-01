c721fad9b933eb533452be44523a642b149e3172
Claus Aichinger <claus.aichinger@mailbox.org>
2022-04-01 13:26:49 +0200

    [tcr] failing: missing_coverage

--- src/game/tkview.py
+++ src/game/tkview.py
@@ -37,6 +37,7 @@ class TkView:
 
         self._snake_objects = {}
         self._arena_objects = {}
+        self._apple_objects = {}
 
     def draw_generic(self, new_points, point_object_mapping, fill, tags):
 
@@ -60,7 +61,7 @@ class TkView:
         self.draw_generic(new_points, self._arena_objects, "gray", ("arena"))
 
     def draw_apple(self, new_points):
-        pass
+        self.draw_generic(new_points, self._apple_objects, "red", ("apple"))
 
     def draw_point(self, point, fill, tags):
         xcenter = self._width / 2 + point.x * 2 * RADIUS
