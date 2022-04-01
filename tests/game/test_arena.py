397c50ff665636c7164a947851067cc3c581c403
Claus Aichinger <claus.aichinger@mailbox.org>
2022-04-01 13:59:42 +0200

    [tcr] working

--- tests/game/test_arena.py
+++ tests/game/test_arena.py
@@ -32,11 +32,11 @@ def test_if_arena_positions_are_occupied(arena3x4):
     assert arena3x4.are_positions_occupied([Point(2, -3), Point(3, -4)]) is True
 
 
-def test_area_has_apple(arena3x4):
+def test_arena_has_apple(arena3x4):
     assert arena3x4.is_position_apple(Point(0, 1)) is True
 
 
-def test_area_can_remove_apple(arena3x4):
+def test_arena_can_remove_apple(arena3x4):
     arena3x4.remove_apple(Point(0, 1))
     assert arena3x4.is_position_apple(Point(0, 1)) is False
 
