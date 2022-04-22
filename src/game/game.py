37d1b815638ee98436c28a5d56da2b142865831c
Claus Aichinger <claus.aichinger@mailbox.org>
2022-04-22 13:55:14 +0200

    [tcr] working

--- src/game/game.py
+++ src/game/game.py
@@ -12,7 +12,7 @@ class Game:
         self._arena = arena
         self._is_running = True
 
-        self._growth = Growth()
+        self._growth = Growth(grow_count=grow_count)
         self._grow_count = grow_count
         self._ticks_to_grow = 0
         self._apple_count = apple_count
