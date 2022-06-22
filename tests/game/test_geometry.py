2a32596e5032aef4c2bcbead282e39a758c937ea
Claus Aichinger <claus.aichinger@mailbox.org>
2022-06-22 13:33:01 +0200

    [tcr] working

--- tests/game/test_geometry.py
+++ tests/game/test_geometry.py
@@ -39,8 +39,7 @@ def test_axis_range():
 
 def test_axis_min():
     axis = Axis(2)
-    axis.min()
-    pass
+    assert axis.min() is None
 
 
 @pytest.mark.parametrize("execution_number", range(100))
