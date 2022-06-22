404e8ec60c3bb9992f974881de9d8d3679571999
Claus Aichinger <claus.aichinger@mailbox.org>
2022-06-22 15:41:32 +0200

    [tcr] revert

--- src/game/game.py
+++ src/game/game.py
@@ -28,7 +28,7 @@ class Game:
 
     def _place_apple(self):
         candidate_apple = self._arena.sample_free_point()
-        while self._arena.is_position_apple(candidate_apple) and False:
+        while self._arena.is_position_apple(candidate_apple):
             # TODO: create apple at suitable position
             # generate random position within walls
             # cf Arena._build_wall for suitable x and y ranges
