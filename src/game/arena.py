be5fde35aad658abf8234b5da433a523b8c512eb
Claus Aichinger <claus.aichinger@mailbox.org>
2022-06-22 13:59:10 +0200

    [tcr] working

--- src/game/arena.py
+++ src/game/arena.py
@@ -13,7 +13,7 @@ from .geometry import Box, Point
 
 class Arena:
     def __init__(self, width, height, box=None, initial_apple=Point(0, 1)):
-        self._box = Box(width, height)
+        self._box = box or Box(width, height)
         self._walls = set()
         self._apples = []
 
