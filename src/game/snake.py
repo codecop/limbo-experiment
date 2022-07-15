d5e84097555bb23b81342974bb7826798f04eb9a
Claus Aichinger <claus.aichinger@mailbox.org>
2022-07-15 14:15:02 +0200

    [tcr] working

--- src/game/snake.py
+++ src/game/snake.py
@@ -35,7 +35,7 @@ from .geometry import Directions, Point
 
 
 class Snake:
-    def __init__(self, tiling=None):
+    def __init__(self, tiling):
         self.direction = Directions.NORTH
         self.body = [Point(0, 0), Point(0, -1), Point(0, -2)]
         self._times_to_grow = 0
