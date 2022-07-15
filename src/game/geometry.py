58557e471c46e106bb64ccfa2ecdd4d1a874e63b
Claus Aichinger <claus.aichinger@mailbox.org>
2022-07-15 14:33:07 +0200

    [tcr] revert

--- src/game/geometry.py
+++ src/game/geometry.py
@@ -22,8 +22,7 @@ class SquareTiling:
 class Point(namedtuple("Point", ["x", "y"])):
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
+
 
 Direction = namedtuple("Direction", ["name", "offset"])
 
