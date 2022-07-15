d9158a9678173e6e72b4e346c14f4fd8a17a55ff
Claus Aichinger <claus.aichinger@mailbox.org>
2022-07-15 14:48:55 +0200

    [tcr] failing: flake8

--- src/game/snake.py
+++ src/game/snake.py
@@ -39,9 +39,11 @@ class Snake:
         self.direction = tiling.starting_direction()
         new_body = []
         center = tiling.origin()
-
+        new_body.append(center)
         second = center - self.direction.offset
+        new_body.append(second)
         third = second - self.direction.offset
+        
         new_body = [center, second, third]
         self.body = new_body
         self._times_to_grow = 0
