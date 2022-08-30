10c7ee8f80baae8e58482fcac524dd1ef9e2412f
Claus Aichinger <claus.aichinger@mailbox.org>
2022-08-30 12:57:48 +0200

    [tcr] working

--- src/game/arena.py
+++ src/game/arena.py
@@ -20,6 +20,7 @@ class Arena:
         self._build_wall()
 
     def _build_wall(self):
+        # TODO NEXT move logic into box, tiling gives us proper box then
         for x in self._box.x.range():
             self._walls.add(Point(x, self._box.y.min()))
             self._walls.add(Point(x, self._box.y.max()))
