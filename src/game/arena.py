3d9371fdc97abe084c8ae79368a4042fb50d067c
Claus Aichinger <claus.aichinger@mailbox.org>
2022-08-30 14:26:21 +0200

    [tcr] revert

--- src/game/arena.py
+++ src/game/arena.py
@@ -21,7 +21,6 @@ class Arena:
 
     def _build_wall(self):
         # TODO NEXT move logic into box, tiling gives us proper box then
-        walls = []
         for x in self._box.x.range():
             self._walls.add(Point(x, self._box.y.min()))
             self._walls.add(Point(x, self._box.y.max()))
