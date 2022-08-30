7e896b12229d98b998f37a2945fd76240b53b9ca
Claus Aichinger <claus.aichinger@mailbox.org>
2022-08-30 14:41:37 +0200

    [tcr] working

--- src/game/arena.py
+++ src/game/arena.py
@@ -15,10 +15,6 @@ class Arena:
         self._box = box
         self._walls = box.boundary_points()
         self._apples = []
-        self._build_wall()
-
-    def _build_wall(self):
-        pass
 
     def place_apple(self, apple):
         self._apples.append(apple)
