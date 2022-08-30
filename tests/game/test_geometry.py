3a908b863089a70f23067586a70c55e39b061127
Claus Aichinger <claus.aichinger@mailbox.org>
2022-08-30 12:36:52 +0200

    [tcr] working

--- tests/game/test_geometry.py
+++ tests/game/test_geometry.py
@@ -49,7 +49,7 @@ def test_axis_random(execution_number):
 
 @pytest.mark.parametrize(
     "current,opposition",
-    [(Directions.NORTH, Directions.SOUTH), (Directions.WEST, Directions.EAST),],
+    [(Directions.NORTH, Directions.SOUTH), (Directions.EAST, Directions.WEST),],
 )
 def test_opposite_direction(current, opposition):
     assert current.opposite() == opposition
