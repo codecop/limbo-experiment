917439f396105cdd1058ca4f38a2e0760d688af5
Claus Aichinger <claus.aichinger@mailbox.org>
2022-08-30 14:23:33 +0200

    [tcr] failing: flake8

--- src/game/arena.py
+++ src/game/arena.py
@@ -12,7 +12,7 @@ from .geometry import Point, SquareTiling
 
 
 class Arena:
-    def __init__(self, box, tiling: SquareTiling = None):
+    def __init__(self, box):
         self._box = box
         self._walls = set()
         self._apples = []
